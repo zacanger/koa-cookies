1931413b1e0c28e382b3df2c100e82526a881403
zacanger <zac@zacanger.com>
2018-09-15 20:39:31 -0600

    add some damn code

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-module.exports = {}
+const defaultNext = () => Promise.resolve()
+
+const setCookie = (name, value, config) =>
+  async (ctx, next = defaultNext) => {
+    ctx.cookies.set(name, value, config)
+    await next()
+  }
+
+const getDefaultConfig = (ctx) => ({
+  domain: ctx.host,
+  maxAge: 1,
+  expires: new Date(1)
+})
+
+const clearCookie = (name, config) =>
+  async (ctx, next = defaultNext) => {
+    ctx.cookies.set(name, 'removed', Object.assign({}, getDefaultConfig(ctx), config))
+    await next()
+  }
+
+module.exports = {
+  setCookie,
+  clearCookie
+}
