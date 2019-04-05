148f1f6b2c0dafff4ec7dff619bfed6b87edb8f3
zacanger <zac@zacanger.com>
2019-04-04 19:15:18 -0600

    Add tests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,54 @@
 const tape = require('tape')
+const http = require('http')
+const Koa = require('koa')
+const request = require('supertest')
 const cookies = require('./')
 
-tape.test('cookies', (t) => {
-  t.ok(cookies, 'fake test')
-  t.end()
+tape.test('set cookie', (t) => {
+  const app = new Koa()
+  app.use(cookies.setCookie('foo', 'bar'))
+  request(http.createServer(app.callback()))
+    .get('/')
+    .expect(200, (err, res) => {
+      t.true(res.headers['set-cookie'][0].includes('foo=bar; path=/;'))
+      t.end()
+    })
+})
+
+tape.test('clear cookie', (t) => {
+  const app = new Koa()
+  app.use(cookies.clearCookie('foo'))
+  request(http.createServer(app.callback()))
+    .get('/')
+    .expect(200, (err, res) => {
+      t.true(res.headers['set-cookie'][0].includes('foo=removed;'))
+      t.end()
+    })
+})
+
+tape.test('config', (t) => {
+  const app = new Koa()
+  const config = {
+    domain: 'example.com'
+  }
+  app.use(cookies.setCookie('foo', 'bar', config))
+  request(http.createServer(app.callback()))
+    .get('/')
+    .expect(200, (err, res) => {
+      t.true(res.headers['set-cookie'][0].includes('domain=example.com;'))
+      t.end()
+    })
+})
+
+tape.test('unicode', (t) => {
+  const app = new Koa()
+  app.use(cookies.setCookie('λ', 'bar'))
+  request(http.createServer(app.callback()))
+    .get('/')
+    .expect(200, (err, res) => {
+      const sc = res.headers['set-cookie'][0]
+      t.true(sc.includes('%CE%BB=b'))
+      t.true(decodeURIComponent(sc).includes('λ'))
+      t.end()
+    })
 })
