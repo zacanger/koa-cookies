8c469a94d1da15b0e4a9ecda6485fc7109e0e768
zacanger <zac@zacanger.com>
2023-09-24 04:20:37 -0600

    chore: rm lib

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as Koa from 'koa';
-declare const defaultNext: () => Promise<void>;
-export declare const setCookie: (s: string, value: any, config?: {}) => (ctx: Koa.Context, next?: () => Promise<void>) => Promise<void>;
-export declare const clearCookie: (s: string, config?: {}) => (ctx: Koa.Context, next?: () => Promise<void>) => Promise<void>;
-export declare const parseCookie: (s?: string) => (ctx: Koa.Context, next?: typeof defaultNext) => Promise<string | undefined>;
-export {};
